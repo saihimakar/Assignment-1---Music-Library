1e1fa4d90eebbf15ef5496e9a6e02c1780fd4cbf
Cris Tamas <leopard2l55@gmail.com>
2019-02-19 18:22:39 -0800

    Updated MainPage with file open, read and write methods

--- MusicLibraryApp/MusicLibraryApp/MainPage.xaml.cs
+++ MusicLibraryApp/MusicLibraryApp/MainPage.xaml.cs
@@ -12,22 +12,102 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Storage.Pickers; 
+using Windows.Storage.Streams;
+using Windows.Storage;
+using System.Text;
+using Windows.UI.ViewManagement;
+using Windows.Media.Playback;
+using Windows.Media.Core;
+//using TagLib;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
-namespace MusicLibraryApp
+namespace App4
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
-            // Added a comment in the Mainpage
-            //added a second comment    
-            this.InitializeComponent(); 
+            this.InitializeComponent();
+            mediaPlayer.TransportControls.IsFullWindowButtonVisible = false;
+            mediaPlayer.TransportControls.IsZoomButtonVisible = false;
+            mediaPlayer.TransportControls.IsStopButtonVisible = true;
+            mediaPlayer.TransportControls.IsStopEnabled = true;
+            ReadPlaylist();
+        }
+        public string[] paths;
+
+        private async void ReadPlaylist()
+        {
+            var file = await ApplicationData.Current.LocalFolder.GetFileAsync("playlist_index.txt");
+            var lines = await FileIO.ReadLinesAsync(file);
+            foreach (var line in lines)
+            {
+                playlist.Items.Add(line);
+            }
+
+        }
+
+
+        private async void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            string filename = "playlist_index.txt";
+            StorageFile FilePath = await ApplicationData.Current.LocalFolder.CreateFileAsync(filename,
+                CreationCollisionOption.OpenIfExists);
+            var openPicker = new FileOpenPicker();
+
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            openPicker.FileTypeFilter.Add(".mp3");
+
+            IReadOnlyList<StorageFile> files = await openPicker.PickMultipleFilesAsync();                       
+
+            foreach (StorageFile file in files)
+            {
+                playlist.Items.Add(file.Path);
+                await FileIO.AppendTextAsync(FilePath, file.Path + Environment.NewLine);
+            }
+
+
+
+
+
+            //FileOpenPicker FileBrowser = new FileOpenPicker();
+            //FileBrowser.SuggestedStartLocation = PickerLocationId.Desktop;
+            //FileBrowser.ViewMode = PickerViewMode.List;
+            //FileBrowser.FileTypeFilter.Add(".mp3");
+            //StorageFile File = await FileBrowser.PickSingleFileAsync();
+            //if (File != null)
+            //{
+            //    IRandomAccessStream stream = await File.OpenAsync(FileAccessMode.Read);
+            //    mediaPlayer.SetSource(stream, File.ContentType);
+            //    mediaPlayer.Play();
+            //}
+
 
         }
     }
-}
+
+
+
+
+
+        //FileOpenPicker FileBrowser = new FileOpenPicker();
+        //FileBrowser.SuggestedStartLocation = PickerLocationId.Desktop;
+        //FileBrowser.ViewMode = PickerViewMode.List;
+        //FileBrowser.FileTypeFilter.Add(".mp3");
+        //StorageFile File = await FileBrowser.PickSingleFileAsync();
+        //if (File != null)
+        //{
+        //    IRandomAccessStream stream = await File.OpenAsync(FileAccessMode.Read);
+        //    media.SetSource(stream, File.ContentType);
+        //    media.Play();
+        //}
+    }
+
+
+
+
+
+    
+
