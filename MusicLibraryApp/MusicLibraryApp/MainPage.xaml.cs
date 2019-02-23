b9e654c3f008395c3d7a2899725f7bd8b072593a
Cris Tamas <leopard2l55@gmail.com>
2019-02-22 17:22:18 -0800

    re-wrote some main components and updated UI

--- MusicLibraryApp/MusicLibraryApp/MainPage.xaml.cs
+++ MusicLibraryApp/MusicLibraryApp/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,46 +14,55 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Windows.Storage.Pickers; 
-using Windows.Storage.Streams;
-using Windows.Storage;
-using System.Text;
-using Windows.UI.ViewManagement;
 using Windows.Media.Playback;
 using Windows.Media.Core;
 using Windows.UI.Popups;
 
 
-namespace App4
+namespace MusicApp1
 {
 
     public sealed partial class MainPage : Page
     {
         public MainPage()
-
         {
+
             this.InitializeComponent();
             mediaPlayer.TransportControls.IsFullWindowButtonVisible = false;
             mediaPlayer.TransportControls.IsZoomButtonVisible = false;
             mediaPlayer.TransportControls.IsStopButtonVisible = true;
             mediaPlayer.TransportControls.IsStopEnabled = true;
-            this.DataContext = DataContext;
-            ReadPlaylist();
-        }
+            this.ViewModel = new RecordingViewModel();
+            LoadPlaylist();
 
+        }
+        public RecordingViewModel ViewModel { get; set; }
         public static IList<string> paths;
         public static StorageFile fileplaylist;
 
-        public async void ReadPlaylist()
+        public async void LoadPlaylist()
         {
             try
             {
-                fileplaylist = await ApplicationData.Current.LocalFolder.GetFileAsync("playlist_index.txt");
+                var fileplaylist = await ApplicationData.Current.LocalFolder.GetFileAsync("playlist_index.txt");
                 var lines = await FileIO.ReadLinesAsync(fileplaylist);
-                
+
                 foreach (var line in lines)
                 {
-                    playlist.Items.Add(line);
+                    StorageFile file = await StorageFile.GetFileFromPathAsync(line);
+                    var musicProperties = await file.Properties.GetMusicPropertiesAsync();
+                    var artist = musicProperties.Artist;
+                    var track = musicProperties.Title;
+                    var album = musicProperties.Album;
+                    var year = musicProperties.Year;
+
+                    ViewModel.recordings.Add(new Recording
+                    {
+                        Artist = artist,
+                        Track = track,
+                        Album = album,
+                        Year = year
+                    });
                 }
             }
             catch (Exception ex)
@@ -59,48 +70,68 @@ namespace App4
             }
         }
 
-        private async void OpenFile_Click(object sender, RoutedEventArgs e)
+
+        private async void BrowseFiles_Click(object sender, RoutedEventArgs e)
         {
+            RecordingViewModel rvm = new RecordingViewModel();
+
             string fileplaylist = "playlist_index.txt";
             StorageFile FilePath = await ApplicationData.Current.LocalFolder.CreateFileAsync(fileplaylist,
                 CreationCollisionOption.OpenIfExists);
+
             var openPicker = new FileOpenPicker();
 
             openPicker.ViewMode = PickerViewMode.Thumbnail;
             openPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
-            openPicker.FileTypeFilter.Add(".mp3");
 
-            IReadOnlyList<StorageFile> files = await openPicker.PickMultipleFilesAsync();                       
+            openPicker.FileTypeFilter.Add(".mp3");
+            openPicker.FileTypeFilter.Add(".wav");
+            openPicker.FileTypeFilter.Add(".m4a");
+            openPicker.FileTypeFilter.Add(".ac3");
+
+            IReadOnlyList<StorageFile> files = await openPicker.PickMultipleFilesAsync();
+
 
             foreach (StorageFile file in files)
             {
-                var musicPoperties = await file.Properties.GetMusicPropertiesAsync();
-                var artist = musicPoperties.Artist;
-                var album = musicPoperties.Album;
-                var title = musicPoperties.Title;                
-                playlist.Items.Add(artist + " - " + title + " - " + album);        
+                var musicProperties = await file.Properties.GetMusicPropertiesAsync();
+                var artist = musicProperties.Artist;
+                var album = musicProperties.Album;
+                var track = musicProperties.Title;
+                var year = musicProperties.Year;
+
+                ViewModel.recordings.Add(new Recording
+                {
+                    Artist = artist,
+                    Track = track,
+                    Album = album,
+                    Year = year
+                });
+
                 await FileIO.AppendTextAsync(FilePath, file.Path + Environment.NewLine);
             }
-                                 
-        }
+       }
 
-        private async void Playlist_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private async void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (playlist.SelectedItem != null)
             {
-                fileplaylist = await ApplicationData.Current.LocalFolder.GetFileAsync("playlist_index.txt");
-                paths = await FileIO.ReadLinesAsync(fileplaylist);
-                StorageFile file = await StorageFile.GetFileFromPathAsync(paths[playlist.SelectedIndex]);
-                var stream = await file.OpenAsync(FileAccessMode.Read);
-                mediaPlayer.SetSource(stream, paths[playlist.SelectedIndex]);
+                if (PlaylistView.SelectedItem != null)
+                {
+                    fileplaylist = await ApplicationData.Current.LocalFolder.GetFileAsync("playlist_index.txt");
+                    paths = await FileIO.ReadLinesAsync(fileplaylist);
+
+                    StorageFile file = await StorageFile.GetFileFromPathAsync(paths[PlaylistView.SelectedIndex]);
+
+                    var stream = await file.OpenAsync(FileAccessMode.Read);
+
+                    mediaPlayer.SetSource(stream, paths[PlaylistView.SelectedIndex]);
+                }
             }
         }
+
+        private void ExitApp_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Exit();
+        }
     }
-    }
-
-
-
-
-
-    
-
+}
---
+++ MusicLibraryApp/MusicLibraryApp/Recording.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicApp1
+{
+    public class Recording
+    {
+            
+        public string Artist { get; set; }
+        public string Track { get; set; }
+        public string Album { get; set; }
+        public uint Year { get; set; }
+        //public Recording()
+        //{
+        //    this.ArtistName = "Wolfgang Amadeus Mozart";
+        //    this.CompositionName = "Andante in C for Piano";
+        //    this.ReleaseDateTime = new DateTime(1761, 1, 1);
+        //}
+        //public string OneLineSummary
+        //{
+        //    get
+        //    {
+        //        return $"{this.CompositionName} by {this.ArtistName}, released: "
+        //            + this.ReleaseDateTime.ToString("d");
+        //    }
+        //}
+    }
+
+    public class RecordingViewModel
+    {
+        //  private Recording defaultRecording = new Recording();
+        //  public Recording DefaultRecording { get { return this.defaultRecording; } }
+        public ObservableCollection<Recording> recordings = new ObservableCollection<Recording>();
+
+        public ObservableCollection<Recording> Recordings { get { return this.recordings; } }
+
+        //public RecordingViewModel()
+        //{
+        //    this.recordings.Add(new Recording()
+        //    {
+        //        Artist = "Johann Sebastian Bach",
+        //        Track = "Mass in B minor",
+        //        Album = "Unknown Album"
+        //    });
+        //}
+    }
+}
